ae7869afab4135de10fbc8c4af640415e5c811e5
noloseqwq <gabrieldjesusperdomo@gmail.com>
2023-12-19 22:23:03 -0400

    ARCHIVOS de CONFIGURACIONES del SISTEMA

---
+++ config/APP.php
@@ -0,0 +1,6 @@
+<?php
+    const SERVER_URL="http://localhost/Sistema-de-Pedidos/";
+
+    const COMPANY="SISTEMA DE PEDIDOS";
+
+    date_default_timezone_set("America/Caracas");
---
+++ config/SERVER.php
@@ -0,0 +1,13 @@
+<?php
+
+    const SERVER="localhost";
+    const DB="sis_pedido";
+    const USER="root";
+    const PASS="";
+
+    const SGBD="mysql:host=".SERVER.";dbname=".DB;
+
+
+    const METHOD="AES-256-CBC";
+    const SECRET_KEY='$ACEITUNAS@1526';
+    const SECRET_IV='30040201';
