90fe163461b34c216f1557152fa59ca49695b47c
nolose <gabrieldjesusperdomo@gmail.com>
2024-02-15 10:55:59 -0400

    MODELO para EVITAR INYECCION SQL

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -22,4 +22,69 @@
             $sql->execute();
             return $sql;
         }
+
+
+        /*-------- Encriptar cadenas --------*/
+            public function encryption($string){
+                $output=FALSE;
+                $key=hash('sha256', SECRET_KEY);
+                $iv=substr(hash('sha256', SECRET_IV), 0, 16);
+                $output=openssl_encrypt($string, METHOD, $key, 0, $iv);
+                $output=base64_encode($output);
+                return $output;
+            }
+
+
+        /*-------- Desencriptar cadenas --------*/
+            protected static function decryption($string){
+                $key=hash('sha256', SECRET_KEY);
+                $iv=substr(hash('sha256', SECRET_IV), 0, 16);
+                $output=openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
+                return $output;
+            }
+
+
+        /*-------- Funcion generar codigos aleatorios --------*/
+        protected static function generar_codigo_aleatorio($letra,$longitud,$numero){
+            for($i=1; $i<=$longitud; $i++){
+                $aleatorio= rand(0,9);
+                $letra.=$aleatorio;
+            }
+            return $letra."-".$numero;
+        } 
+
+
+        /*-------- Funcion limpiar cadenas --------*/
+        protected static function limpiar_cadena($cadena){
+            $cadena=trim($cadena);
+            $cadena=stripslashes($cadena);
+            $cadena=str_ireplace("<script>", "", $cadena );
+            $cadena=str_ireplace("</script>", "", $cadena );
+            $cadena=str_ireplace("<script src", "", $cadena );
+            $cadena=str_ireplace("<script type=", "", $cadena );
+            $cadena=str_ireplace("SELECT * FROM", "", $cadena );
+            $cadena=str_ireplace("DELETE * FROM", "", $cadena );
+            $cadena=str_ireplace("INSERT INTO", "", $cadena );
+            $cadena=str_ireplace("DROP TABLE", "", $cadena );
+            $cadena=str_ireplace("DROP DATABASE", "", $cadena );
+            $cadena=str_ireplace("TRUNCATE TABLE", "", $cadena );
+            $cadena=str_ireplace("SHOW TABLES", "", $cadena );
+            $cadena=str_ireplace("SHOW DATABASES", "", $cadena );
+            $cadena=str_ireplace("<?php", "", $cadena );
+            $cadena=str_ireplace("?>", "", $cadena );
+            $cadena=str_ireplace("--", "", $cadena );
+            $cadena=str_ireplace(">", "", $cadena );
+            $cadena=str_ireplace("<", "", $cadena );
+            $cadena=str_ireplace("[", "", $cadena );
+            $cadena=str_ireplace("]", "", $cadena );
+            $cadena=str_ireplace("^", "", $cadena );
+            $cadena=str_ireplace("==", "", $cadena );
+            $cadena=str_ireplace(";", "", $cadena );
+            $cadena=str_ireplace("::", "", $cadena );
+            $cadena=stripslashes($cadena);
+            $cadena=trim($cadena);
+            
+        }
+        
+
     }
