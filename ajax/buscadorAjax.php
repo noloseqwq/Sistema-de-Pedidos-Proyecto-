591450ba2bbd8d5248736c07979e03ae718a8e14
nolose <gabrieldjesusperdomo@gmail.com>
2024-04-08 10:57:39 -0400

    Crando el buscador de usuarios

---
+++ ajax/buscadorAjax.php
@@ -0,0 +1,95 @@
+<?php
+session_start(['name' => 'SDP']);
+require_once "../config/APP.php";
+
+if(isset($_POST['busqueda_inicial']) || isset($_POST['eliminar_busqueda']) || isset($_POST['fecha_creacion']) ){
+
+    $data_URL=[
+        "usuario"=> "user-search",
+        "cliente"=> "client-search",
+        "producto"=>"product-search",
+        "pedido"=>"order-search"
+    ];
+
+    if(isset($_POST['modulo'])){
+        $modulo=$_POST['modulo'];
+        if(!isset($data_URL[$modulo])){
+            $alerta=[
+                "Alerta"=>"simple",
+                "Titulo"=>"Ocurrio un error inesperado",
+                "Texto"=>"No podemos continuar con la busquedad debido a un error",
+                "Tipo"=>"error"
+            ];
+            echo json_encode($alerta);
+            exit();
+        }
+    }else{
+        $alerta=[
+            "Alerta"=>"simple",
+            "Titulo"=>"Ocurrio un error inesperado",
+            "Texto"=>"No podemos continuar con la busquedad",
+            "Tipo"=>"error"
+        ];
+        echo json_encode($alerta);
+        exit();
+    }
+
+    if($modulo=="pedido"){
+        $fecha_creacion="fecha_creacion_".$modulo;
+        /* iniciar busquedad*/
+        if(isset($_POST['fecha_creacion']) ){
+
+            if($_POST['fecha_creacion']==""){       
+                $alerta=[
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrio un error inesperado",
+                    "Texto"=>"llene los campos requeridos",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+            $_SESSION[$fecha_creacion]= $_POST['fecha_creacion'];
+        }
+
+        /*Eliminar busqueda*/        
+        if(isset($_POST['eliminar_busqueda'])){
+            unset($_SESSION[$fecha_creacion]);
+        }
+
+    }else{
+        $name_var= "busquedad_".$modulo;
+        /* iniciar busquedad*/
+        if(isset($_POST['busqueda_inicial'])){
+            if($_POST['busqueda_inicial']==""){
+                $alerta=[
+                    "Alerta"=>"simple",
+                    "Titulo"=>"Ocurrio un error inesperado",
+                    "Texto"=>"llene los campos requeridos",
+                    "Tipo"=>"error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+            $_SESSION[$name_var]=$_POST['busqueda_inicial'];
+        }
+        /*Eliminar busqueda*/        
+        if(isset($_POST['eliminar_busqueda'])){
+            unset($_SESSION[$name_var]);
+        }
+    }
+
+    //redireccionar
+    $URL=$data_URL[$modulo];
+    $alerta=[
+        "Alerta"=>"redireccionar",
+        "URL"=>SERVER_URL.$URL."/"
+    ];
+    echo json_encode($alerta);
+
+
+}else{
+    session_unset();
+    session_destroy();
+    header("Location: ".SERVER_URL."login/");
+}
--- vistas/contenidos/user-search-CJ.php
+++ vistas/contenidos/user-search-CJ.php
@@ -27,14 +27,18 @@ if ($_SESSION['privilegio_sdp'] != 1) {
 </div>
 
 <!-- Content -->
+<?php
+	if(!isset($_SESSION['busquedad_usuario'])&& empty ($_SESSION['busquedad_usuario'])){
+?>
 <div class="container-fluid">
-	<form class="form-neon" action="">
+	<form class="form-neon FormularioAjax" action="<?php echo SERVER_URL; ?>ajax/buscadorAjax.php" method="POST" data-form="default" autocomplete="off">
+		<input type="hidden" name="modulo" value="usuario">
 		<div class="container-fluid">
 			<div class="row justify-content-md-center">
 				<div class="col-12 col-md-6">
 					<div class="form-group">
 						<label for="inputSearch" class="bmd-label-floating">¿Qué usuario estas buscando?</label>
-						<input type="text" class="form-control" name="busqueda-" id="inputSearch" maxlength="30">
+						<input type="text" class="form-control" name="busqueda_inicial" id="inputSearch" maxlength="30">
 					</div>
 				</div>
 				<div class="col-12">
@@ -42,19 +46,21 @@ if ($_SESSION['privilegio_sdp'] != 1) {
 						<button type="submit" class="btn btn-raised btn-info"><i class="fas fa-search"></i> &nbsp; BUSCAR</button>
 					</p>
 				</div>
+				
 			</div>
 		</div>
 	</form>
 </div>
-
+<?php }else{?>
 <div class="container-fluid">
-	<form action="">
-		<input type="hidden" name="eliminar-busqueda" value="eliminar">
+<form class="FormularioAjax" action="<?php echo SERVER_URL; ?>ajax/buscadorAjax.php" method="POST" data-form="search" autocomplete="off">
+		<input type="hidden" name="modulo" value="usuario">
+		<input type="hidden" name="eliminar_busqueda" value="eliminar">
 		<div class="container-fluid">
 			<div class="row justify-content-md-center">
 				<div class="col-12 col-md-6">
 					<p class="text-center" style="font-size: 20px;">
-						Resultados de la busqueda <strong>“Buscar”</strong>
+						Resultados de la busqueda <strong>"<?php echo $_SESSION['busquedad_usuario'];?>"</strong>
 					</p>
 				</div>
 				<div class="col-12">
@@ -68,120 +74,14 @@ if ($_SESSION['privilegio_sdp'] != 1) {
 </div>
 
 <div class="container-fluid">
-	<div class="table-responsive">
-		<table class="table table-dark table-sm">
-			<thead>
-				<tr class="text-center roboto-medium">
-					<th>#</th>
-					<th>DNI</th>
-					<th>NOMBRE</th>
-					<th>APELLIDO</th>
-					<th>TELÉFONO</th>
-					<th>USUARIO</th>
-					<th>EMAIL</th>
-					<th>ACTUALIZAR</th>
-					<th>ELIMINAR</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr class="text-center">
-					<td>1</td>
-					<td>03045643</td>
-					<td>NOMBRE DE USUARIO</td>
-					<td>APELLIDO DE USUARIO</td>
-					<td>2345456</td>
-					<td>NOMBRE DE USUARIO</td>
-					<td>ADMIN@ADMIN.COM</td>
-					<td>
-						<a href="user-update.html" class="btn btn-success">
-							<i class="fas fa-sync-alt"></i>
-						</a>
-					</td>
-					<td>
-						<form action="">
-							<button type="button" class="btn btn-warning">
-								<i class="far fa-trash-alt"></i>
-							</button>
-						</form>
-					</td>
-				</tr>
-				<tr class="text-center">
-					<td>2</td>
-					<td>03045643</td>
-					<td>NOMBRE DE USUARIO</td>
-					<td>APELLIDO DE USUARIO</td>
-					<td>2345456</td>
-					<td>NOMBRE DE USUARIO</td>
-					<td>ADMIN@ADMIN.COM</td>
-					<td>
-						<a href="user-update.html" class="btn btn-success">
-							<i class="fas fa-sync-alt"></i>
-						</a>
-					</td>
-					<td>
-						<form action="">
-							<button type="button" class="btn btn-warning">
-								<i class="far fa-trash-alt"></i>
-							</button>
-						</form>
-					</td>
-				</tr>
-				<tr class="text-center">
-					<td>3</td>
-					<td>03045643</td>
-					<td>NOMBRE DE USUARIO</td>
-					<td>APELLIDO DE USUARIO</td>
-					<td>2345456</td>
-					<td>NOMBRE DE USUARIO</td>
-					<td>ADMIN@ADMIN.COM</td>
-					<td>
-						<a href="user-update.html" class="btn btn-success">
-							<i class="fas fa-sync-alt"></i>
-						</a>
-					</td>
-					<td>
-						<form action="">
-							<button type="button" class="btn btn-warning">
-								<i class="far fa-trash-alt"></i>
-							</button>
-						</form>
-					</td>
-				</tr>
-				<tr class="text-center">
-					<td>4</td>
-					<td>03045643</td>
-					<td>NOMBRE DE USUARIO</td>
-					<td>APELLIDO DE USUARIO</td>
-					<td>2345456</td>
-					<td>NOMBRE DE USUARIO</td>
-					<td>ADMIN@ADMIN.COM</td>
-					<td>
-						<a href="user-update.html" class="btn btn-success">
-							<i class="fas fa-sync-alt"></i>
-						</a>
-					</td>
-					<td>
-						<form action="">
-							<button type="button" class="btn btn-warning">
-								<i class="far fa-trash-alt"></i>
-							</button>
-						</form>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-	<nav aria-label="Page navigation example">
-		<ul class="pagination justify-content-center">
-			<li class="page-item disabled">
-				<a class="page-link" href="#" tabindex="-1">Previous</a>
-			</li>
-			<li class="page-item"><a class="page-link" href="#">1</a></li>
-			<li class="page-item"><a class="page-link" href="#">2</a></li>
-			<li class="page-item"><a class="page-link" href="#">3</a></li>
-			<li class="page-item">
-				<a class="page-link" href="#">Next</a>
-			</li>
-		</ul>
-	</nav>
+<?php
+					require_once "./controladores/usuarioControlador.php";
+					$ins_usuarioTab = new usuarioControlador();
+					
+					echo $ins_usuarioTab->paginador_usuario_controlador($pagina[1],8, $_SESSION['privilegio_sdp'],$_SESSION['id_sdp'],$pagina[0],$_SESSION['busquedad_usuario']);
+
+
+				?>
 </div>
+<?php } ?>
+
